456738bef58689b83fc4cbd68dc7eaaaf46cbcf2
Rovindra Kumar <robinku07@gmail.com>
2017-11-02 12:38:09 +0000

    Some Modification

--- chef-repo/cookbooks/apache/recipes/apache.rb
+++ chef-repo/cookbooks/apache/recipes/apache.rb
@@ -1,9 +1,16 @@
+if node['platform_family']=='rhel'
+	apache='httpd'
+	apache-service='httpd'
+elsif node['platform_family']=='debian'
+	apache='apache2'
+	apache-service='apache2'
+end	
 package 'apache2' do
-	package_name 'httpd'
+	package_name apache
 	notifies :start,'service[apache2]',:immediately
 end
 service 'apache2' do
-	service_name 'httpd'
+	service_name apache-service
 	action :nothing
 end
 file 'index_file' do
